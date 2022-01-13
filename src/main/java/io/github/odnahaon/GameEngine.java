69accf36094864978da28cbf0ed2fa3ab33edfb2
odnahaon <noah.paul.ando@gmail.com>
2022-01-13 13:13:16 -0800

    Started programming

---
+++ src/main/java/io/github/odnahaon/GameEngine.java
@@ -0,0 +1,39 @@
+package io.github.odnahaon;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class GameEngine {
+    private long window;
+
+    public void run() {
+        init();
+        loop();
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+    }
+
+    private void loop() {
+
+    }
+
+    public static void main(String[] args) {
+        new GameEngine().run();
+    }
+}
